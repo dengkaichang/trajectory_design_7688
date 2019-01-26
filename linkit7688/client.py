abfd687b9da1ef0a9df3b0f4e8250f9f7521e2dc
dengkai <wl01420395@gmail.com>
2019-01-26 16:09:37 +0800

    [linkit7688/client.py] socket connection [linkit7688/socket_client.py] socket connection [linkit7688/config.ini] socket connection

--- linkit7688/client.py
+++ linkit7688/client.py
@@ -1,13 +1,43 @@
 import socket
+import sys
+import threading
+import time
 
 import ConfigParser
 config = ConfigParser.RawConfigParser()
 config.read('config.ini')
 HOST = config.get('MYSERVER', 'HOST')
 PORT = int(config.get('MYSERVER', 'PORT'))
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
-response = client.recv(4096)
-print response
+
+sys.path.insert(0, '/usr/lib/python2.7/bridge/')  
+
+from bridgeclient import BridgeClient as bridgeclient
+value = bridgeclient()
+
+h0 = ""
+
+def pipe_form_arduino():
+	while True:
+		global h0
+		h0 = value.get("msg")
+		if h0 == None:
+			h0 = "empty" 
+
+def send_to_server():
+	while True:
+		global h0
+		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		client.connect((HOST, PORT))
+		client.send(str(h0))
+		time.sleep(1)
+
+t1 = threading.Thread(target = pipe_form_arduino)
+t2 = threading.Thread(target = send_to_server)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
--- server/server.py
+++ server/server.py
@@ -19,12 +19,12 @@ def handle_client(client_socket):
     request = client_socket.recv(1024)
     print "[*] Received: %s" % request
     
-    client_socket.send("ACK!")
+    # client_socket.send("ACK!")
     client_socket.close()
     
 while True:
     client, addr = server.accept()
     print "[*] Acepted connection from: %s:%d" % (addr[0],addr[1])
-    
+
     client_handler = threading.Thread(target=handle_client, args=(client,))
     client_handler.start()
